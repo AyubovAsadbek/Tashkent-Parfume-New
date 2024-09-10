4c2b806706f77a1306f4b5fa3c92784bf805af52
Asadullohbek05 <ayubovasadbek33@gmail.com>
2024-09-10 12:12:30 +0500

    feat: next config dir

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 // @ts-check
 
-const path = require('path')
+const path = require("path");
 const withNextIntl = require("next-intl/plugin")();
 
 /** @type {import('next').NextConfig} */
 const config = {
-    webpack(config) {
-        config.resolve.alias['form'] = path.resolve(__dirname, 'components/form');
-        
-        return config;
-      },
+  webpack(config) {
+    config.resolve.alias["form"] = path.resolve(__dirname, "components/form");
+
+    return config;
+  },
 };
 
 module.exports = withNextIntl(config);
